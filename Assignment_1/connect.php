4ed8536e4b3d28ead71de8dc0a5ca7005665fbb6
Pallavi Dhawan <pallavidhawan72@gmail.com>
2025-07-09 15:48:06 -0400

    Delete Assignment_1 directory

--- Assignment_1/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$connect= mysqli_connect('localhost', 'PaulDhawan', 'Paulie@123', 'designer_tracker');
-
-    if(!$connect){
-        die("Connection Failed: " . mysqli_connect_error());
-    }
